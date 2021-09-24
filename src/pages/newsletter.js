695bc94d2b07bccffb718dc8e977c3287dddd2a4
Esau Silva <esausilva85@gmail.com>
2021-09-23 19:13:17 -0500

    Rename Subscribe to Newsletter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 import { MainLayout } from '~components/layouts/MainLayout';
 import { SecondaryLayout } from '~components/layouts/SecondaryLayout';
-import { Subscribe as SubscribeComponent } from '~components/pages/Subscribe';
+import { Subscribe } from '~components/pages/Subscribe';
 
-const Subscribe = () => {
+const Newsletter = () => {
   return (
     <MainLayout pageTitle="Subscribe to My Newsletter" pathName="subscribe">
       <SecondaryLayout>
-        <SubscribeComponent />
+        <Subscribe />
       </SecondaryLayout>
     </MainLayout>
   );
 };
 
-export default Subscribe;
+export default Newsletter;
